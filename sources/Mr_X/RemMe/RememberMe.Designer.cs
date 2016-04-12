2eb8c74642317fac6c2be0a98e40aeb66835482a
onlylove97 <songchichatdat97@gmail.com>
2016-04-12 17:23:46 +0700

    Hoàn thiện game và thêm comment vào game

--- sources/Mr_X/RemMe/RememberMe.Designer.cs
+++ sources/Mr_X/RemMe/RememberMe.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RememberMe));
             this.pnlGameDisplayGray = new System.Windows.Forms.Panel();
+            this.btnMediate = new System.Windows.Forms.Button();
+            this.wmpSoundTrack = new AxWMPLib.AxWindowsMediaPlayer();
             this.pnlGameDisplayYellow = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlGameDisplayGreen = new System.Windows.Forms.Panel();
+            this.lblOpportunity = new System.Windows.Forms.Label();
+            this.txtOpportunity = new System.Windows.Forms.TextBox();
+            this.txtSelectedColor = new System.Windows.Forms.TextBox();
+            this.lblSelectedColor = new System.Windows.Forms.Label();
+            this.txtYoung_Score = new System.Windows.Forms.TextBox();
+            this.txtScoreToPass = new System.Windows.Forms.TextBox();
+            this.lblScoreToPass = new System.Windows.Forms.Label();
+            this.lblYoungScore = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.nudSecond = new System.Windows.Forms.NumericUpDown();
+            this.nudMinute = new System.Windows.Forms.NumericUpDown();
             this.btnEnd = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
+            this.tmrTimeToWatch = new System.Windows.Forms.Timer(this.components);
+            this.tmrTimeToPlay = new System.Windows.Forms.Timer(this.components);
+            this.pnlGameDisplayGray.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpSoundTrack)).BeginInit();
+            this.pnlGameDisplayYellow.SuspendLayout();
             this.pnlGameDisplayGreen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlGameDisplayGray
@@ -42,27 +66,84 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlGameDisplayGray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.pnlGameDisplayGray.Controls.Add(this.btnMediate);
+            this.pnlGameDisplayGray.Controls.Add(this.wmpSoundTrack);
             this.pnlGameDisplayGray.ForeColor = System.Drawing.Color.Black;
             this.pnlGameDisplayGray.Location = new System.Drawing.Point(4, 4);
             this.pnlGameDisplayGray.Name = "pnlGameDisplayGray";
             this.pnlGameDisplayGray.Size = new System.Drawing.Size(809, 654);
             this.pnlGameDisplayGray.TabIndex = 0;
             // 
+            // btnMediate
+            // 
+            this.btnMediate.Enabled = false;
+            this.btnMediate.Location = new System.Drawing.Point(107, 599);
+            this.btnMediate.Name = "btnMediate";
+            this.btnMediate.Size = new System.Drawing.Size(126, 48);
+            this.btnMediate.TabIndex = 1;
+            this.btnMediate.Text = "btnMediate";
+            this.btnMediate.UseVisualStyleBackColor = true;
+            this.btnMediate.Visible = false;
+            this.btnMediate.Click += new System.EventHandler(this.btnMediate_Click);
+            // 
+            // wmpSoundTrack
+            // 
+            this.wmpSoundTrack.Enabled = true;
+            this.wmpSoundTrack.Location = new System.Drawing.Point(0, 628);
+            this.wmpSoundTrack.Name = "wmpSoundTrack";
+            this.wmpSoundTrack.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpSoundTrack.OcxState")));
+            this.wmpSoundTrack.Size = new System.Drawing.Size(75, 23);
+            this.wmpSoundTrack.TabIndex = 0;
+            this.wmpSoundTrack.Visible = false;
+            // 
             // pnlGameDisplayYellow
             // 
             this.pnlGameDisplayYellow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlGameDisplayYellow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.pnlGameDisplayYellow.Controls.Add(this.textBox1);
+            this.pnlGameDisplayYellow.Controls.Add(this.label1);
             this.pnlGameDisplayYellow.ForeColor = System.Drawing.Color.Black;
             this.pnlGameDisplayYellow.Location = new System.Drawing.Point(818, 4);
             this.pnlGameDisplayYellow.Name = "pnlGameDisplayYellow";
             this.pnlGameDisplayYellow.Size = new System.Drawing.Size(395, 292);
             this.pnlGameDisplayYellow.TabIndex = 0;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 175);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(338, 99);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Ban đầu các ô đều có màu nhất định. Sau 5s, tất cả các ô đều sẽ có màu đỏ. Nhiệm " +
+    "vụ của bạn là chọn tất cả các ô có màu ban đầu trùng với màu được chọn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Trò chơi ghi nhớ";
+            // 
             // pnlGameDisplayGreen
             // 
             this.pnlGameDisplayGreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlGameDisplayGreen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.pnlGameDisplayGreen.Controls.Add(this.lblOpportunity);
+            this.pnlGameDisplayGreen.Controls.Add(this.txtOpportunity);
+            this.pnlGameDisplayGreen.Controls.Add(this.txtSelectedColor);
+            this.pnlGameDisplayGreen.Controls.Add(this.lblSelectedColor);
+            this.pnlGameDisplayGreen.Controls.Add(this.txtYoung_Score);
+            this.pnlGameDisplayGreen.Controls.Add(this.txtScoreToPass);
+            this.pnlGameDisplayGreen.Controls.Add(this.lblScoreToPass);
+            this.pnlGameDisplayGreen.Controls.Add(this.lblYoungScore);
+            this.pnlGameDisplayGreen.Controls.Add(this.lblTime);
+            this.pnlGameDisplayGreen.Controls.Add(this.nudSecond);
+            this.pnlGameDisplayGreen.Controls.Add(this.nudMinute);
             this.pnlGameDisplayGreen.Controls.Add(this.btnEnd);
             this.pnlGameDisplayGreen.Controls.Add(this.btnStart);
             this.pnlGameDisplayGreen.ForeColor = System.Drawing.Color.Black;
@@ -70,6 +151,103 @@
             this.pnlGameDisplayGreen.Name = "pnlGameDisplayGreen";
             this.pnlGameDisplayGreen.Size = new System.Drawing.Size(395, 358);
             this.pnlGameDisplayGreen.TabIndex = 1;
+            // 
+            // lblOpportunity
+            // 
+            this.lblOpportunity.AutoSize = true;
+            this.lblOpportunity.Location = new System.Drawing.Point(1, 143);
+            this.lblOpportunity.Name = "lblOpportunity";
+            this.lblOpportunity.Size = new System.Drawing.Size(102, 20);
+            this.lblOpportunity.TabIndex = 17;
+            this.lblOpportunity.Text = "Số lượt chọn:";
+            // 
+            // txtOpportunity
+            // 
+            this.txtOpportunity.Enabled = false;
+            this.txtOpportunity.Location = new System.Drawing.Point(149, 137);
+            this.txtOpportunity.Name = "txtOpportunity";
+            this.txtOpportunity.ReadOnly = true;
+            this.txtOpportunity.Size = new System.Drawing.Size(100, 26);
+            this.txtOpportunity.TabIndex = 16;
+            // 
+            // txtSelectedColor
+            // 
+            this.txtSelectedColor.Enabled = false;
+            this.txtSelectedColor.Location = new System.Drawing.Point(149, 34);
+            this.txtSelectedColor.Name = "txtSelectedColor";
+            this.txtSelectedColor.ReadOnly = true;
+            this.txtSelectedColor.Size = new System.Drawing.Size(100, 26);
+            this.txtSelectedColor.TabIndex = 15;
+            // 
+            // lblSelectedColor
+            // 
+            this.lblSelectedColor.AutoSize = true;
+            this.lblSelectedColor.Location = new System.Drawing.Point(3, 40);
+            this.lblSelectedColor.Name = "lblSelectedColor";
+            this.lblSelectedColor.Size = new System.Drawing.Size(122, 20);
+            this.lblSelectedColor.TabIndex = 14;
+            this.lblSelectedColor.Text = "Màu được chọn:";
+            // 
+            // txtYoung_Score
+            // 
+            this.txtYoung_Score.Enabled = false;
+            this.txtYoung_Score.Location = new System.Drawing.Point(149, 70);
+            this.txtYoung_Score.Name = "txtYoung_Score";
+            this.txtYoung_Score.ReadOnly = true;
+            this.txtYoung_Score.Size = new System.Drawing.Size(100, 26);
+            this.txtYoung_Score.TabIndex = 13;
+            // 
+            // txtScoreToPass
+            // 
+            this.txtScoreToPass.Enabled = false;
+            this.txtScoreToPass.Location = new System.Drawing.Point(149, 105);
+            this.txtScoreToPass.Name = "txtScoreToPass";
+            this.txtScoreToPass.ReadOnly = true;
+            this.txtScoreToPass.Size = new System.Drawing.Size(100, 26);
+            this.txtScoreToPass.TabIndex = 12;
+            // 
+            // lblScoreToPass
+            // 
+            this.lblScoreToPass.AutoSize = true;
+            this.lblScoreToPass.Location = new System.Drawing.Point(3, 108);
+            this.lblScoreToPass.Name = "lblScoreToPass";
+            this.lblScoreToPass.Size = new System.Drawing.Size(140, 20);
+            this.lblScoreToPass.TabIndex = 10;
+            this.lblScoreToPass.Text = "Số điểm qua vòng:";
+            // 
+            // lblYoungScore
+            // 
+            this.lblYoungScore.AutoSize = true;
+            this.lblYoungScore.Location = new System.Drawing.Point(3, 75);
+            this.lblYoungScore.Name = "lblYoungScore";
+            this.lblYoungScore.Size = new System.Drawing.Size(111, 20);
+            this.lblYoungScore.TabIndex = 9;
+            this.lblYoungScore.Text = "Điểm của bạn:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(3, 169);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(81, 20);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Thời gian :";
+            // 
+            // nudSecond
+            // 
+            this.nudSecond.Enabled = false;
+            this.nudSecond.Location = new System.Drawing.Point(208, 167);
+            this.nudSecond.Name = "nudSecond";
+            this.nudSecond.Size = new System.Drawing.Size(41, 26);
+            this.nudSecond.TabIndex = 3;
+            // 
+            // nudMinute
+            // 
+            this.nudMinute.Enabled = false;
+            this.nudMinute.Location = new System.Drawing.Point(149, 167);
+            this.nudMinute.Name = "nudMinute";
+            this.nudMinute.Size = new System.Drawing.Size(41, 26);
+            this.nudMinute.TabIndex = 2;
             // 
             // btnEnd
             // 
@@ -80,6 +258,7 @@
             this.btnEnd.TabIndex = 1;
             this.btnEnd.Text = "Kết thúc";
             this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
             // 
             // btnStart
             // 
@@ -90,6 +269,17 @@
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Bắt đầu";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmrTimeToWatch
+            // 
+            this.tmrTimeToWatch.Interval = 1000;
+            this.tmrTimeToWatch.Tick += new System.EventHandler(this.tmrTimeToWatch_Tick);
+            // 
+            // tmrTimeToPlay
+            // 
+            this.tmrTimeToPlay.Interval = 1000;
+            this.tmrTimeToPlay.Tick += new System.EventHandler(this.tmrTimeToPlay_Tick);
             // 
             // RememberMe
             // 
@@ -107,8 +297,16 @@
             this.Name = "RememberMe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RememberMe";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RememberMe_FormClosed);
             this.Load += new System.EventHandler(this.RememberMe_Load);
+            this.pnlGameDisplayGray.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpSoundTrack)).EndInit();
+            this.pnlGameDisplayYellow.ResumeLayout(false);
+            this.pnlGameDisplayYellow.PerformLayout();
             this.pnlGameDisplayGreen.ResumeLayout(false);
+            this.pnlGameDisplayGreen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -120,5 +318,22 @@
         private System.Windows.Forms.Panel pnlGameDisplayGreen;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnEnd;
+        private AxWMPLib.AxWindowsMediaPlayer wmpSoundTrack;
+        private System.Windows.Forms.Button btnMediate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudSecond;
+        private System.Windows.Forms.NumericUpDown nudMinute;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblYoungScore;
+        private System.Windows.Forms.Label lblScoreToPass;
+        private System.Windows.Forms.TextBox txtScoreToPass;
+        private System.Windows.Forms.TextBox txtYoung_Score;
+        private System.Windows.Forms.Label lblSelectedColor;
+        private System.Windows.Forms.TextBox txtSelectedColor;
+        private System.Windows.Forms.Label lblOpportunity;
+        private System.Windows.Forms.TextBox txtOpportunity;
+        private System.Windows.Forms.Timer tmrTimeToWatch;
+        private System.Windows.Forms.Timer tmrTimeToPlay;
     }
 }
--- sources/Mr_X/RemMe/RememberMe.cs
+++ sources/Mr_X/RemMe/RememberMe.cs
@@ -7,19 +7,227 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace RemMe
 {
     public partial class RememberMe : Form
     {
-        public RememberMe()
+        private int sizeTable;
+        private int numberOfAnswer; // so luong dap an cua tro choi
+        private int numberOfChoice; // so luong luot choi cua nguoi choi
+        private int minute = 0;
+        private int second = 5;
+        private int selectedColor; // vi tri cua mau duoc chon trong mang ArrayColor
+        private readonly Button[][] randomBtn = new Button[30][]; 
+        private string[] arrayColor = new string[10]; // mang cung cap hex code cho cac mau
+        private int[] number = new int[100]; // number[i]=so luong o co mau i trong bang
+        private int location;  
+        private int value;   
+        private bool[] checkColor = new bool[100 + 10];
+        private int YoungScore=0; // diem so cua nguoi choi
+        private int[] original = new int[1000 + 10]; // original[i]=mau ban dau cua button co ten=i
+        private int timeToPlay; // thoi gian choi
+        
+        // ham lay ngau nhien 1 so
+        private int randomNumber(int limitLow, int limitHigh)
+        {
+            Random rd = new Random();
+            return rd.Next(limitLow, limitHigh + 1);
+        }
+
+        // lay cac gia tri: kich thuoc cua bang, so dap an cua game, so luot duoc lua chon va thoi gian choi
+        public RememberMe(int size, int numAns,int numChoice, int time)
         {
             InitializeComponent();
+            sizeTable = size;
+            numberOfAnswer = numAns;
+            numberOfChoice = numChoice;
+            timeToPlay = time;
+            wmpSoundTrack.URL = @"sound/RemMe/SoundTrack.mp3";
         }
 
         private void RememberMe_Load(object sender, EventArgs e)
         {
 
+            arrayColor[1] = "#33CCFF";
+            arrayColor[2] = "#355C96";
+            arrayColor[3] = "#9370db";
+            arrayColor[4] = "#EE3233";
+            selectedColor = randomNumber(1, 3);
+            if (selectedColor == 1)
+            {
+                number[1] = numberOfAnswer;
+                number[2] = (sizeTable * sizeTable - number[1]) / 2;
+                number[3] = sizeTable * sizeTable - number[1] - number[2];
+            }
+            else if (selectedColor == 2)
+            {
+                number[2] = numberOfAnswer;
+                number[1] = (sizeTable * sizeTable - number[2]) / 2;
+                number[3] = sizeTable * sizeTable - number[1] - number[2];
+            }
+            else
+            {
+                number[3] = numberOfAnswer;
+                number[1] = (sizeTable * sizeTable - number[3]) / 2;
+                number[2] = sizeTable * sizeTable - number[1] - number[3];
+            }
+            for (int i = 1; i <= sizeTable * sizeTable; i++) checkColor[i] = false;
+        }
+
+        // tao mang button: ten, kich thuoc, vi tri, mau sac ban dau
+        private void createButtonArray()
+        {
+            for (var i = 1; i <= sizeTable; i++) randomBtn[i] = new Button[30];
+            for (var i = 1; i <= sizeTable; i++)
+                for (var j = 1; j <= sizeTable; j++)
+                {
+                    value=(i-1)*sizeTable+j;
+                    randomBtn[i][j] = new Button();
+                    randomBtn[i][j].Name = value.ToString();
+                    randomBtn[i][j].Size = new Size(55, 55);
+                    randomBtn[i][j].FlatStyle = FlatStyle.Flat;
+                    randomBtn[i][j].Click += new EventHandler(btnMediate_Click);
+                    Controls.Add(randomBtn[i][j]);
+                    randomBtn[i][j].Enabled = false;
+                    randomBtn[i][j].Location = new Point(i * 60 + 20, j * 60 + 20);
+                   
+                    location = randomNumber(1, 3);
+                    while (number[location] == 0 && location < 3) location++;
+                    while (number[location] == 0 && location >1 ) location--;
+                    number[location]--;
+                    if (location == selectedColor) checkColor[value] = true;
+                    original[value] = location;
+
+                    randomBtn[i][j].BackColor = ColorTranslator.FromHtml(arrayColor[location]);
+                 
+                    pnlGameDisplayGray.Controls.Add(randomBtn[i][j]);
+                }
+        }
+
+        // bat dau tro choi
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            createButtonArray();
+            tmrTimeToWatch.Enabled = true;
+            btnStart.Enabled = false;
+            txtSelectedColor.BackColor = ColorTranslator.FromHtml(arrayColor[selectedColor]);
+        }
+
+        // dem nguoc thoi gian nguoi choi co the xem mau ban dau cua cac button
+        private void tmrTimeToWatch_Tick(object sender, EventArgs e)
+        {
+            txtYoung_Score.Text = YoungScore.ToString();
+            txtScoreToPass.Text = numberOfAnswer.ToString();
+            txtOpportunity.Text = numberOfChoice.ToString();
+            nudMinute.Value = minute;
+            nudSecond.Value = second;
+            if (minute > 0 || second > 0)
+            {
+                if (second == 0 && minute > 0)
+                {
+                    second = 60;
+                    minute--;
+                }
+                second--;
+            }
+            else
+            {
+                tmrTimeToWatch.Stop();
+                afterWatch();
+            }
+        }
+
+        // khoi tao sau khi het thoi gian xem
+        private void afterWatch()
+        {
+            for (int i = 1; i <= sizeTable; i++)
+                for (int j = 1; j <= sizeTable; j++)
+                {
+                    randomBtn[i][j].Enabled = true;
+                    randomBtn[i][j].BackColor = ColorTranslator.FromHtml(arrayColor[4]);
+                }
+            minute = timeToPlay / 60;
+            second = timeToPlay % 60;
+            tmrTimeToPlay.Enabled = true;
+        }
+
+        // dem nguoc thoi gian nguoi choi co the chon dap an
+        private void tmrTimeToPlay_Tick(object sender, EventArgs e)
+        {
+            txtYoung_Score.Text = YoungScore.ToString();
+            txtScoreToPass.Text = numberOfAnswer.ToString();
+            txtOpportunity.Text = numberOfChoice.ToString();
+            nudMinute.Value = minute;
+            nudSecond.Value = second;
+            if ((minute > 0 || second > 0) && numberOfChoice>0 )
+            {
+                if (second == 0 && minute > 0)
+                {
+                    second = 60;
+                    minute--;
+                }
+                second--;
+            }
+            else
+            {
+                tmrTimeToPlay.Stop();
+                answer();
+            }
+        }
+        // xu li khi nguoi choi chon vao button
+        private void btnMediate_Click(object sender, EventArgs e)
+        {
+            numberOfChoice--;
+            Button btnMedia = (Button)sender;
+            value = Int32.Parse(btnMedia.Name);
+            btnMedia.BackColor = ColorTranslator.FromHtml(arrayColor[original[value]]);
+            if (checkColor[value])
+            {
+                YoungScore++;
+                SoundPlayer sp = new SoundPlayer(@"sound/RemMe/happy.wav");
+                sp.Play();
+                btnMedia.Visible = false;
+            }
+            else
+            {
+                SoundPlayer sp = new SoundPlayer(@"sound/RemMe/sad.wav");
+                sp.Play();
+            }
+        }
+
+        //dua ra cau tra loi cho nguoi choi, va hien lai mang ban dau
+        private void answer()
+        {
+            for (int i = 1; i <= sizeTable; i++)
+                for (int j = 1; j <= sizeTable; j++)
+                {
+                    randomBtn[i][j].Enabled = false;
+                    value = Int32.Parse(randomBtn[i][j].Name);
+                    randomBtn[i][j].BackColor = ColorTranslator.FromHtml(arrayColor[original[value]]);
+                }
+
+            if (YoungScore>= numberOfAnswer)
+            {
+                MessageBox.Show("Chúc mừng bạn đã vượt qua trò chơi này", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Rất tiếc bạn đã không vượt qua trò chơi này", "Thông báo");
+            }
+        }
+        
+        //tat game
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // tat nhac khi dong chuong trinh
+        private void RememberMe_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            wmpSoundTrack.close();
         }
     }
 }
