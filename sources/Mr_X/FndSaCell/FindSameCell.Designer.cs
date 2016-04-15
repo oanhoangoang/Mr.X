9c0d4c8da9123560a39be206f2ee59294c1d3bb2
onlylove97 <songchichatdat97@gmail.com>
2016-04-15 23:34:19 +0700

    hoàn thành và cập nhật icon+sound game FndSaCell cho Mr_X

--- sources/Mr_X/FndSaCell/FindSameCell.Designer.cs
+++ sources/Mr_X/FndSaCell/FindSameCell.Designer.cs
@@ -29,10 +29,259 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FindSameCell));
+            this.pnlGameDisplayGray = new System.Windows.Forms.Panel();
+            this.btnMediate = new System.Windows.Forms.Button();
+            this.wmpSoundTrack = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pnlGameDisplaySunFlower = new System.Windows.Forms.Panel();
+            this.pnlGameDisplayBlue = new System.Windows.Forms.Panel();
+            this.txtOpportunity = new System.Windows.Forms.TextBox();
+            this.txtScoreToPass = new System.Windows.Forms.TextBox();
+            this.txtYourScore = new System.Windows.Forms.TextBox();
+            this.lblOpportunity = new System.Windows.Forms.Label();
+            this.lblScoreToPass = new System.Windows.Forms.Label();
+            this.lblYourScore = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblRule = new System.Windows.Forms.Label();
+            this.nudSecond = new System.Windows.Forms.NumericUpDown();
+            this.nudMinute = new System.Windows.Forms.NumericUpDown();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrTimeToPlay = new System.Windows.Forms.Timer(this.components);
+            this.pnlGameDisplayGray.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpSoundTrack)).BeginInit();
+            this.pnlGameDisplayBlue.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlGameDisplayGray
+            // 
+            this.pnlGameDisplayGray.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlGameDisplayGray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.pnlGameDisplayGray.Controls.Add(this.btnMediate);
+            this.pnlGameDisplayGray.Controls.Add(this.wmpSoundTrack);
+            this.pnlGameDisplayGray.Location = new System.Drawing.Point(4, 4);
+            this.pnlGameDisplayGray.Name = "pnlGameDisplayGray";
+            this.pnlGameDisplayGray.Size = new System.Drawing.Size(809, 654);
+            this.pnlGameDisplayGray.TabIndex = 0;
+            // 
+            // btnMediate
+            // 
+            this.btnMediate.Enabled = false;
+            this.btnMediate.Location = new System.Drawing.Point(81, 606);
+            this.btnMediate.Name = "btnMediate";
+            this.btnMediate.Size = new System.Drawing.Size(106, 45);
+            this.btnMediate.TabIndex = 1;
+            this.btnMediate.Text = "btnMediate";
+            this.btnMediate.UseVisualStyleBackColor = true;
+            this.btnMediate.Visible = false;
+            this.btnMediate.Click += new System.EventHandler(this.btnMediate_Click);
+            // 
+            // wmpSoundTrack
+            // 
+            this.wmpSoundTrack.Enabled = true;
+            this.wmpSoundTrack.Location = new System.Drawing.Point(0, 606);
+            this.wmpSoundTrack.Name = "wmpSoundTrack";
+            this.wmpSoundTrack.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpSoundTrack.OcxState")));
+            this.wmpSoundTrack.Size = new System.Drawing.Size(75, 48);
+            this.wmpSoundTrack.TabIndex = 0;
+            this.wmpSoundTrack.Visible = false;
+            // 
+            // pnlGameDisplaySunFlower
+            // 
+            this.pnlGameDisplaySunFlower.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlGameDisplaySunFlower.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.pnlGameDisplaySunFlower.Location = new System.Drawing.Point(818, 4);
+            this.pnlGameDisplaySunFlower.Name = "pnlGameDisplaySunFlower";
+            this.pnlGameDisplaySunFlower.Size = new System.Drawing.Size(395, 292);
+            this.pnlGameDisplaySunFlower.TabIndex = 1;
+            // 
+            // pnlGameDisplayBlue
+            // 
+            this.pnlGameDisplayBlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlGameDisplayBlue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(171)))), ((int)(((byte)(227)))));
+            this.pnlGameDisplayBlue.Controls.Add(this.txtOpportunity);
+            this.pnlGameDisplayBlue.Controls.Add(this.txtScoreToPass);
+            this.pnlGameDisplayBlue.Controls.Add(this.txtYourScore);
+            this.pnlGameDisplayBlue.Controls.Add(this.lblOpportunity);
+            this.pnlGameDisplayBlue.Controls.Add(this.lblScoreToPass);
+            this.pnlGameDisplayBlue.Controls.Add(this.lblYourScore);
+            this.pnlGameDisplayBlue.Controls.Add(this.lblTime);
+            this.pnlGameDisplayBlue.Controls.Add(this.lblRule);
+            this.pnlGameDisplayBlue.Controls.Add(this.nudSecond);
+            this.pnlGameDisplayBlue.Controls.Add(this.nudMinute);
+            this.pnlGameDisplayBlue.Controls.Add(this.btnEnd);
+            this.pnlGameDisplayBlue.Controls.Add(this.btnStart);
+            this.pnlGameDisplayBlue.Location = new System.Drawing.Point(818, 300);
+            this.pnlGameDisplayBlue.Name = "pnlGameDisplayBlue";
+            this.pnlGameDisplayBlue.Size = new System.Drawing.Size(395, 358);
+            this.pnlGameDisplayBlue.TabIndex = 2;
+            // 
+            // txtOpportunity
+            // 
+            this.txtOpportunity.Location = new System.Drawing.Point(276, 180);
+            this.txtOpportunity.Name = "txtOpportunity";
+            this.txtOpportunity.ReadOnly = true;
+            this.txtOpportunity.Size = new System.Drawing.Size(70, 26);
+            this.txtOpportunity.TabIndex = 11;
+            this.txtOpportunity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtScoreToPass
+            // 
+            this.txtScoreToPass.Location = new System.Drawing.Point(153, 180);
+            this.txtScoreToPass.Name = "txtScoreToPass";
+            this.txtScoreToPass.ReadOnly = true;
+            this.txtScoreToPass.Size = new System.Drawing.Size(70, 26);
+            this.txtScoreToPass.TabIndex = 10;
+            this.txtScoreToPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtYourScore
+            // 
+            this.txtYourScore.Location = new System.Drawing.Point(37, 180);
+            this.txtYourScore.Name = "txtYourScore";
+            this.txtYourScore.ReadOnly = true;
+            this.txtYourScore.Size = new System.Drawing.Size(70, 26);
+            this.txtYourScore.TabIndex = 9;
+            this.txtYourScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblOpportunity
+            // 
+            this.lblOpportunity.AutoSize = true;
+            this.lblOpportunity.Location = new System.Drawing.Point(262, 157);
+            this.lblOpportunity.Name = "lblOpportunity";
+            this.lblOpportunity.Size = new System.Drawing.Size(98, 20);
+            this.lblOpportunity.TabIndex = 8;
+            this.lblOpportunity.Text = "Số lượt chọn";
+            // 
+            // lblScoreToPass
+            // 
+            this.lblScoreToPass.AutoSize = true;
+            this.lblScoreToPass.Location = new System.Drawing.Point(125, 157);
+            this.lblScoreToPass.Name = "lblScoreToPass";
+            this.lblScoreToPass.Size = new System.Drawing.Size(115, 20);
+            this.lblScoreToPass.TabIndex = 7;
+            this.lblScoreToPass.Text = "Điểm qua vòng";
+            // 
+            // lblYourScore
+            // 
+            this.lblYourScore.AutoSize = true;
+            this.lblYourScore.Location = new System.Drawing.Point(43, 157);
+            this.lblYourScore.Name = "lblYourScore";
+            this.lblYourScore.Size = new System.Drawing.Size(46, 20);
+            this.lblYourScore.TabIndex = 6;
+            this.lblYourScore.Text = "Điểm";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(106, 102);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(77, 20);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Thời gian:";
+            // 
+            // lblRule
+            // 
+            this.lblRule.AutoSize = true;
+            this.lblRule.Location = new System.Drawing.Point(33, 19);
+            this.lblRule.Name = "lblRule";
+            this.lblRule.Size = new System.Drawing.Size(278, 20);
+            this.lblRule.TabIndex = 4;
+            this.lblRule.Text = "Trò chơi: Tìm các ô có hình giống nhau";
+            // 
+            // nudSecond
+            // 
+            this.nudSecond.Enabled = false;
+            this.nudSecond.Location = new System.Drawing.Point(254, 100);
+            this.nudSecond.Name = "nudSecond";
+            this.nudSecond.Size = new System.Drawing.Size(57, 26);
+            this.nudSecond.TabIndex = 3;
+            // 
+            // nudMinute
+            // 
+            this.nudMinute.Enabled = false;
+            this.nudMinute.Location = new System.Drawing.Point(192, 100);
+            this.nudMinute.Name = "nudMinute";
+            this.nudMinute.Size = new System.Drawing.Size(56, 26);
+            this.nudMinute.TabIndex = 2;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnd.Location = new System.Drawing.Point(226, 270);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(105, 60);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "Kết thúc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Location = new System.Drawing.Point(59, 270);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 60);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmrTimeToPlay
+            // 
+            this.tmrTimeToPlay.Interval = 1000;
+            this.tmrTimeToPlay.Tick += new System.EventHandler(this.tmrTimeToPlay_Tick);
+            // 
+            // FindSameCell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1217, 663);
+            this.Controls.Add(this.pnlGameDisplayBlue);
+            this.Controls.Add(this.pnlGameDisplaySunFlower);
+            this.Controls.Add(this.pnlGameDisplayGray);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FindSameCell";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FindSameCell";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FindSameCell_FormClosed);
+            this.pnlGameDisplayGray.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpSoundTrack)).EndInit();
+            this.pnlGameDisplayBlue.ResumeLayout(false);
+            this.pnlGameDisplayBlue.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlGameDisplayGray;
+        private System.Windows.Forms.Panel pnlGameDisplaySunFlower;
+        private System.Windows.Forms.Panel pnlGameDisplayBlue;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.NumericUpDown nudSecond;
+        private System.Windows.Forms.NumericUpDown nudMinute;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblYourScore;
+        private System.Windows.Forms.Label lblScoreToPass;
+        private System.Windows.Forms.Label lblOpportunity;
+        private System.Windows.Forms.TextBox txtOpportunity;
+        private System.Windows.Forms.TextBox txtScoreToPass;
+        private System.Windows.Forms.TextBox txtYourScore;
+        private System.Windows.Forms.Label lblRule;
+        private AxWMPLib.AxWindowsMediaPlayer wmpSoundTrack;
+        private System.Windows.Forms.Button btnMediate;
+        private System.Windows.Forms.Timer tmrTimeToPlay;
     }
 }
--- sources/Mr_X/FndSaCell/FindSameCell.cs
+++ sources/Mr_X/FndSaCell/FindSameCell.cs
@@ -7,14 +7,267 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Media;
 
 namespace FndSaCell
 {
     public partial class FindSameCell : Form
     {
-        public FindSameCell()
+        // tạo button tròn
+        class circleButton : System.Windows.Forms.Button
+        {
+            protected override void OnPaint(PaintEventArgs pevent)
+            {
+                GraphicsPath gp = new GraphicsPath();
+                gp.AddEllipse(0, 0, ClientSize.Height, ClientSize.Width);
+                this.Region = new Region(gp);
+                base.OnPaint(pevent);
+            }
+        }
+
+        // tạo mảng button hiển thị các con vật
+        private circleButton[][] btnCircle = new circleButton[100][];
+
+        //số phút chơi
+        private int minute;
+
+        // số giây chơi
+        private int second;
+
+        // chiều cao của bảng
+        private int heightTable;
+
+        // chiều rộng của bảng
+        private int widthTable;
+
+        // diện tích của bảng
+        private int sizeTable;
+
+        // điểm qua vòng
+        private int scoreToPass;
+
+        // số lượng lượt chọn
+        private int numberOfChoice;
+
+        // điểm người chơi
+        private int yourScore;
+
+        // tên của button
+        private int value;
+
+        // numberOfIcon[i]= số lượng icon thứ i còn có thể sử dụng, mặc định được khởi tạo bằng 2
+        private int[] numberOfIcon = new int[1000 + 10];
+
+        // lấy ngẫu nhiên vị trí của một icon mà icon đó xuất hiện chưa xuất hiện đủ 2 lần
+        private int location;
+
+        // original[i]= đường dẫn tới icon của button tên i
+        private string[] original = new String[100 + 10];
+
+        // tính số lần nhấn của người chơi
+        private int numberOfClick;
+
+        // lưu tên của button đã nhấn liền trước đó
+        private int previousButton;
+
+        // lấy các giá trị: chiều cao của bảng, chiều rộng của bảng, số điểm qua vòng,số lượt nhấn, thời gian chơi
+        public FindSameCell(int height, int width, int Score, int numChoice, int time)
         {
             InitializeComponent();
+            heightTable = height;
+            widthTable = width;
+            if (heightTable % 2 == 1 && widthTable % 2 == 1) heightTable--;
+            sizeTable = height * width;
+            scoreToPass = Score;
+            numberOfChoice = numChoice;
+            minute = time / 60;
+            second = time % 60;
+            try
+            {
+                wmpSoundTrack.URL = @"sound/FndSaCell/SoundTrack.mp3";
+            }
+            catch (Exception ex) { }
+        }
+
+        // lấy ngẫu nhiên một số trong khoảng từ limitLow tới limitHigh
+        private int randomNumber(int limitLow, int limitHigh)
+        {
+            Random rd = new Random();
+            return rd.Next(limitLow, limitHigh + 1);
+        }
+
+        //tạo mảng button tròn
+        private void createCircleButtonArray()
+        {
+            for (int i = 1; i <= sizeTable / 2; i++) numberOfIcon[i] = 2;
+            for (var i = 1; i <= sizeTable; i++) btnCircle[i] = new circleButton[30];
+
+            for (int i = 1; i <= heightTable; i++)
+                for (int j = 1; j <= widthTable; j++)
+                {
+                    value = (i - 1) * widthTable + j;
+                    btnCircle[i][j] = new circleButton();
+
+                    btnCircle[i][j].Size = new Size(63, 63);
+                    btnCircle[i][j].Location = new Point(i * 70 + 20, j * 70 + 20);
+
+                    btnCircle[i][j].Name = value.ToString();
+                    btnCircle[i][j].FlatAppearance.BorderSize = 0;
+                    btnCircle[i][j].FlatStyle = FlatStyle.Flat;
+                    btnCircle[i][j].BackColor = ColorTranslator.FromHtml("#87D37C");
+
+                    btnCircle[i][j].Click += new EventHandler(btnMediate_Click);
+                    Controls.Add(btnCircle[i][j]);
+                    pnlGameDisplayGray.Controls.Add(btnCircle[i][j]);
+
+                    location = randomNumber(1, sizeTable / 2 );
+                    while (numberOfIcon[location] == 0 && location < sizeTable/2) location++;
+                    while (numberOfIcon[location] == 0 && location > 1) location--;
+                    numberOfIcon[location]--;
+
+                    original[value] = "picture/FndSaCell/" + location.ToString() + ".png";
+                }
+        }
+
+        // bắt đầu trò chơi
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            createCircleButtonArray();
+            yourScore = 0;
+            previousButton = 0;
+            numberOfClick = 0;
+            tmrTimeToPlay.Enabled = true;
+            btnStart.Enabled = false;
+        }
+
+        // đếm ngược thời gian chơi
+        private void tmrTimeToPlay_Tick(object sender, EventArgs e)
+        {
+            txtYourScore.Text = yourScore.ToString();
+            txtScoreToPass.Text = scoreToPass.ToString();
+            txtOpportunity.Text = numberOfChoice.ToString();
+            nudMinute.Value = minute;
+            nudSecond.Value = second;
+
+            if (minute > 0 || second > 0 && numberOfChoice > 0)
+            {
+                if (second == 0 && minute > 0)
+                {
+                    second = 60;
+                    minute--;
+                }
+                second--;
+            }
+            else
+            {
+                tmrTimeToPlay.Stop();
+                answer();
+            }
+        }
+
+        //xử lí khi người chơi nhấn vào button
+        private void btnMediate_Click(object sender, EventArgs e)
+        {
+            numberOfClick++;
+            numberOfChoice--;
+            circleButton btnMedia = (circleButton)sender;
+            try
+            {
+                btnMedia.Image = Image.FromFile(@original[Int32.Parse(btnMedia.Name)]);
+            }
+            catch (Exception ex) { }
+
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(300);
+            });
+            t.Wait();
+
+            if (numberOfClick % 2 == 0)
+            {
+                if (original[Int32.Parse(btnMedia.Name)] == original[previousButton])
+                {
+                    yourScore += 2;
+                    try
+                    {
+                        SoundPlayer sp = new SoundPlayer(@"sound/FndSaCell/happy.wav");
+                        sp.Play();
+                    }
+                    catch (Exception ex) { }
+                    btnMedia.Visible = false;
+                    for (int i = 1; i <= heightTable; i++)
+                        for (int j = 1; j <= widthTable; j++)
+                            if (Int32.Parse(btnCircle[i][j].Name) == previousButton)
+                            {
+                                btnCircle[i][j].Visible = false;
+                                break;
+                            }
+                }
+                else
+                {
+                    try
+                    {
+                        SoundPlayer sp = new SoundPlayer(@"sound/FndSaCell/sad.wav");
+                        sp.Play();
+                    }
+                    catch (Exception ex) { }
+                    btnMedia.Image = null;
+                    for (int i = 1; i <= heightTable; i++)
+                        for (int j = 1; j <= widthTable; j++)
+                            if (Int32.Parse(btnCircle[i][j].Name) == previousButton)
+                            {
+                                btnCircle[i][j].Image = null;
+                                btnCircle[i][j].Click += new EventHandler(btnMediate_Click);
+                                break;
+                            }
+
+                }
+            }
+            else
+            {
+                previousButton = Int32.Parse(btnMedia.Name);
+                btnMedia.Click -= new EventHandler(btnMediate_Click);
+                try
+                {
+                    SoundPlayer sp = new SoundPlayer(@"sound/FndSaCell/openButton.wav");
+                    sp.Play();
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        // trả về kết quả của người chơi
+        private void answer()
+        {
+            for (int i = 1; i <= heightTable; i++)
+                for (int j = 1; j <= widthTable; j++)
+                {
+                    btnCircle[i][j].Image = Image.FromFile(@original[Int32.Parse(btnCircle[i][j].Name)]);
+                    btnCircle[i][j].Click -= new EventHandler(btnMediate_Click);
+                }
+            if (yourScore >= scoreToPass)
+            {
+                MessageBox.Show("Chúc mừng bạn đã vượt qua thử thách này", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Rất tiếc bạn đã không vượt qua thử thách này. Chúc may mắn", "Thông báo");
+            }
+        }
+
+        //tắt game
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            tmrTimeToPlay.Stop();
+            this.Close();
+        }
+
+        // tắt nhạc khi đóng chương trình
+        private void FindSameCell_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            wmpSoundTrack.close();
         }
     }
 }
