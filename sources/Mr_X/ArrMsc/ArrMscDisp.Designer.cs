87231ffcc243b5aad3b45c5137514b517e1d66a8
toet251 <toet.37@gmail.com>
2016-05-08 10:13:18 +0700

    Update minigame ArrMsc

---
+++ sources/Mr_X/ArrMsc/ArrMscDisp.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace ArrMsc
+{
+    partial class ArrMscDisp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArrMscDisp));
+            this.pnlGameDisp = new System.Windows.Forms.Panel();
+            this.txtNoti = new System.Windows.Forms.TextBox();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.pnlPlayerInfo = new System.Windows.Forms.Panel();
+            this.lblRankData = new System.Windows.Forms.Label();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.lblLevelData = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.pnlGameInfo = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimeUnit = new System.Windows.Forms.Label();
+            this.lblTimeCount = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrPlaySound = new System.Windows.Forms.Timer(this.components);
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.pnlGameDisp.SuspendLayout();
+            this.pnlPlayerInfo.SuspendLayout();
+            this.pnlGameInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlGameDisp
+            // 
+            this.pnlGameDisp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(131)))), ((int)(((byte)(215)))));
+            this.pnlGameDisp.Controls.Add(this.txtNoti);
+            this.pnlGameDisp.Controls.Add(this.btn4);
+            this.pnlGameDisp.Controls.Add(this.btn3);
+            this.pnlGameDisp.Controls.Add(this.btn2);
+            this.pnlGameDisp.Controls.Add(this.btn1);
+            this.pnlGameDisp.Controls.Add(this.btn0);
+            this.pnlGameDisp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlGameDisp.ForeColor = System.Drawing.Color.White;
+            this.pnlGameDisp.Location = new System.Drawing.Point(4, 4);
+            this.pnlGameDisp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnlGameDisp.Name = "pnlGameDisp";
+            this.pnlGameDisp.Size = new System.Drawing.Size(809, 654);
+            this.pnlGameDisp.TabIndex = 0;
+            // 
+            // txtNoti
+            // 
+            this.txtNoti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(131)))), ((int)(((byte)(215)))));
+            this.txtNoti.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNoti.ForeColor = System.Drawing.Color.White;
+            this.txtNoti.Location = new System.Drawing.Point(137, 109);
+            this.txtNoti.Name = "txtNoti";
+            this.txtNoti.Size = new System.Drawing.Size(567, 23);
+            this.txtNoti.TabIndex = 15;
+            this.txtNoti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(449, 451);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(110, 90);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "Btn 4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(285, 451);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(110, 90);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "Btn 3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(525, 310);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(110, 90);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "Btn 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(131)))), ((int)(((byte)(215)))));
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(199, 310);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(110, 90);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Btn 1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(362, 182);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(110, 90);
+            this.btn0.TabIndex = 4;
+            this.btn0.Text = "Btn 0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // pnlPlayerInfo
+            // 
+            this.pnlPlayerInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlPlayerInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.pnlPlayerInfo.Controls.Add(this.lblRankData);
+            this.pnlPlayerInfo.Controls.Add(this.lblRank);
+            this.pnlPlayerInfo.Controls.Add(this.lblLevelData);
+            this.pnlPlayerInfo.Controls.Add(this.lblLevel);
+            this.pnlPlayerInfo.ForeColor = System.Drawing.Color.White;
+            this.pnlPlayerInfo.Location = new System.Drawing.Point(818, 4);
+            this.pnlPlayerInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnlPlayerInfo.Name = "pnlPlayerInfo";
+            this.pnlPlayerInfo.Size = new System.Drawing.Size(395, 292);
+            this.pnlPlayerInfo.TabIndex = 1;
+            // 
+            // lblRankData
+            // 
+            this.lblRankData.AutoSize = true;
+            this.lblRankData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRankData.Location = new System.Drawing.Point(185, 159);
+            this.lblRankData.Name = "lblRankData";
+            this.lblRankData.Size = new System.Drawing.Size(42, 25);
+            this.lblRankData.TabIndex = 3;
+            this.lblRankData.Text = "xxx";
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRank.Location = new System.Drawing.Point(68, 159);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(85, 25);
+            this.lblRank.TabIndex = 2;
+            this.lblRank.Text = "Chức vụ";
+            // 
+            // lblLevelData
+            // 
+            this.lblLevelData.AutoSize = true;
+            this.lblLevelData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevelData.Location = new System.Drawing.Point(185, 109);
+            this.lblLevelData.Name = "lblLevelData";
+            this.lblLevelData.Size = new System.Drawing.Size(30, 25);
+            this.lblLevelData.TabIndex = 1;
+            this.lblLevelData.Text = "lvl";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.Location = new System.Drawing.Point(68, 109);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(59, 25);
+            this.lblLevel.TabIndex = 0;
+            this.lblLevel.Text = "Level";
+            // 
+            // pnlGameInfo
+            // 
+            this.pnlGameInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.pnlGameInfo.Controls.Add(this.btnBack);
+            this.pnlGameInfo.Controls.Add(this.btnStart);
+            this.pnlGameInfo.Controls.Add(this.lblTimeUnit);
+            this.pnlGameInfo.Controls.Add(this.lblTimeCount);
+            this.pnlGameInfo.Controls.Add(this.lblTime);
+            this.pnlGameInfo.ForeColor = System.Drawing.Color.White;
+            this.pnlGameInfo.Location = new System.Drawing.Point(818, 300);
+            this.pnlGameInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnlGameInfo.Name = "pnlGameInfo";
+            this.pnlGameInfo.Size = new System.Drawing.Size(395, 358);
+            this.pnlGameInfo.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(212, 168);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(106, 77);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Trở về";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(81, 168);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(106, 77);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTimeUnit
+            // 
+            this.lblTimeUnit.AutoSize = true;
+            this.lblTimeUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeUnit.Location = new System.Drawing.Point(213, 101);
+            this.lblTimeUnit.Name = "lblTimeUnit";
+            this.lblTimeUnit.Size = new System.Drawing.Size(52, 25);
+            this.lblTimeUnit.TabIndex = 2;
+            this.lblTimeUnit.Text = "Giây";
+            // 
+            // lblTimeCount
+            // 
+            this.lblTimeCount.AutoSize = true;
+            this.lblTimeCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeCount.Location = new System.Drawing.Point(178, 101);
+            this.lblTimeCount.Name = "lblTimeCount";
+            this.lblTimeCount.Size = new System.Drawing.Size(23, 25);
+            this.lblTimeCount.TabIndex = 1;
+            this.lblTimeCount.Text = "0";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(68, 101);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(104, 25);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Thời gian :";
+            // 
+            // tmrPlaySound
+            // 
+            this.tmrPlaySound.Interval = 1000;
+            this.tmrPlaySound.Tick += new System.EventHandler(this.tmrPlaySound_Tick);
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 1000;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // ArrMscDisp
+            // 
+            this.ClientSize = new System.Drawing.Size(1217, 663);
+            this.Controls.Add(this.pnlGameInfo);
+            this.Controls.Add(this.pnlPlayerInfo);
+            this.Controls.Add(this.pnlGameDisp);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ArrMscDisp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mr. X";
+            this.Load += new System.EventHandler(this.ArrMscDisp_Load);
+            this.pnlGameDisp.ResumeLayout(false);
+            this.pnlGameDisp.PerformLayout();
+            this.pnlPlayerInfo.ResumeLayout(false);
+            this.pnlPlayerInfo.PerformLayout();
+            this.pnlGameInfo.ResumeLayout(false);
+            this.pnlGameInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlGameDisp;
+        private System.Windows.Forms.Panel pnlPlayerInfo;
+        private System.Windows.Forms.Label lblRankData;
+        private System.Windows.Forms.Label lblRank;
+        private System.Windows.Forms.Label lblLevelData;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Panel pnlGameInfo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimeUnit;
+        private System.Windows.Forms.Label lblTimeCount;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Timer tmrPlaySound;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.TextBox txtNoti;
+    }
+}
---
+++ sources/Mr_X/ArrMsc/ArrMscDisp.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ArrMsc
+{
+    public partial class ArrMscDisp : Form
+    {
+        // Infomation of Player and Game
+        int lvlGame; 
+        string rank, lvl;
+
+        // Check variable: Check = 0 Lose; Check = 1 Win
+        int check = 0; 
+
+        // Time (second), number of notes (n), count variable
+        int sec;
+        int n;
+        int count = 0;
+
+        // Arrays of Note sound and Note name
+        private string[] linkNote = new string[7] { "sound/ArrMsc/1.wav", "sound/ArrMsc/2.wav", "sound/ArrMsc/3.wav", "sound/ArrMsc/4.wav", "sound/ArrMsc/5.wav", "sound/ArrMsc/6.wav", "sound/ArrMsc/7.wav" };
+        private string[] note = new string[7] {"Do","Re","Mi","Fa","Sol","La","Si"};
+
+        //SoundPlayer soundtrack = new SoundPlayer(@"sound/ArrMsc/Soundtrack1.wav");
+        SoundPlayer soundWin = new SoundPlayer(@"sound/ArrMsc/Win.wav");
+        SoundPlayer soundLose = new SoundPlayer(@"sound/ArrMsc/Lose.wav");
+ 
+        private int[] a = new int[5];
+        private int[] noteAt = new int[5] { 0, 0, 0, 0, 0 };
+        private int[] dd = new int[5] { 0, 0, 0, 0, 0};
+        
+        // Strings to save the answers
+        string strAns = "", strPlrAns = "";
+        
+
+        //
+        // Call Form
+        //
+        public ArrMscDisp(int xlvlGame, string xRank, string xLvl) 
+        {
+            InitializeComponent();
+            lvlGame = xlvlGame;
+            rank = xRank;
+            lvl = xLvl;
+        }
+
+        //
+        // Prepare to play
+        //
+        private void ArrMscDisp_Load(object sender, EventArgs e)
+        {
+            lblLevelData.Text = lvl;
+            lblRankData.Text = rank;
+            btn0.Visible = false; 
+            btn1.Visible = false; 
+            btn2.Visible = false; 
+            btn3.Visible = false; 
+            btn4.Visible = false;
+            txtNoti.Text = "Hãy sắp xếp các nốt theo thứ tự bạn nghe được :)";
+            if (lvlGame == 1) n = 3; else n = 5;
+            initData();
+        }
+
+        //
+        // Initialize
+        //
+        private void initData()
+        {
+            Random rd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                // random Note
+                a[i] = rd.Next(100) % 7;
+                strAns = strAns + a[i].ToString();
+                
+                // random Note position button
+                int x;
+                do
+                {
+                    x = rd.Next(n);
+                }
+                while ( dd[x] == 1 || x >=n );
+                dd[x] = 1; noteAt[x] = a[i];
+                
+                switch (x) 
+                {
+                    case 0: btn0.Text = note[a[i]]; break;
+                    case 1: btn1.Text = note[a[i]]; break;
+                    case 2: btn2.Text = note[a[i]]; break;
+                    case 3: btn3.Text = note[a[i]]; break;
+                    case 4: btn4.Text = note[a[i]]; break;
+                }
+            }
+        }
+
+        //
+        // play Sound
+        //
+        private void playMusic(string link)
+        {
+            try
+            {
+                SoundPlayer sp = new SoundPlayer(@link);
+                sp.Play();
+            }
+            catch (Exception ex) { }
+        }
+
+        //
+        // Click button Start
+        // 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btn0.Visible = true; btn0.Enabled = false;
+            btn1.Visible = true; btn1.Enabled = false;
+            btn2.Visible = true; btn2.Enabled = false;
+            if (n == 5) { btn3.Visible = true; btn3.Enabled = false; }
+            if (n == 5) { btn4.Visible = true; btn4.Enabled = false; }
+            sec = -1;
+            tmrPlaySound.Start();
+        }
+
+        //
+        // Play the Sound
+        //
+        private void tmrPlaySound_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec == 0) playMusic( @linkNote[a[0]] );
+            if (sec == 1) playMusic( @linkNote[a[1]] );
+            if (sec == 2) playMusic( @linkNote[a[2]] );
+            if (sec == 3 && n == 5) playMusic( @linkNote[a[3]] );
+            if (sec == 4 && n == 5) playMusic( @linkNote[a[4]] );
+            if (sec == n+1) txtNoti.Text = "Bây giờ bạn sẽ có " + (n*3).ToString() + " giây để hoàn thành ^^";
+            if (sec == n+3)
+            {
+                tmrPlaySound.Stop();                
+                txtNoti.Text = "Thời gian BẮT ĐẦU. Nhanh tay lên nào !";
+                sec = n * 3; tmr.Start();
+                btn0.Enabled = true;
+                btn1.Enabled = true;
+                btn2.Enabled = true;
+                btn3.Enabled = true;
+                btn4.Enabled = true;
+            }
+        }
+
+        //
+        // Timer to Play
+        //
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            lblTimeCount.Text = sec.ToString();
+            sec--;
+            if (sec == -1) notiWrong();
+        }
+
+        //
+        // Check the Answer
+        //
+        private void checkAnswer()
+        {
+            lblTimeCount.Text = "0";
+            if ( strPlrAns == strAns ) notiRight();
+            else notiWrong();
+        }
+
+        //
+        // Announce the Answer
+        //
+        private void notiRight()
+        {
+            tmr.Stop(); check = 1;
+            soundWin.Play();
+            txtNoti.Text = "Thật tuyệt vời, bạn quá là xuất sắc :))";
+        }
+
+        private void notiWrong()
+        {
+            tmr.Stop(); check = 0;
+            soundLose.Play();
+            txtNoti.Text = "Thua mất rồi, lần sau cố gắng lên nhé :<";
+        }
+
+        //
+        // Click the Answer Button
+        //
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            strPlrAns = strPlrAns + noteAt[0].ToString();
+            btn0.BackColor = System.Drawing.Color.FromArgb(82, 179, 217);
+            btn0.Enabled = false; count++; if (count == n) checkAnswer();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            strPlrAns = strPlrAns + noteAt[1].ToString();
+            btn1.BackColor = System.Drawing.Color.FromArgb(82, 179, 217);
+            btn1.Enabled = false; count++; if (count == n) checkAnswer();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            strPlrAns = strPlrAns + noteAt[2].ToString();
+            btn2.BackColor = System.Drawing.Color.FromArgb(82, 179, 217);
+            btn2.Enabled = false; count++; if (count == n) checkAnswer();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            strPlrAns = strPlrAns + noteAt[3].ToString();
+            btn3.BackColor = System.Drawing.Color.FromArgb(82, 179, 217);
+            btn3.Enabled = false; count++; if (count == n) checkAnswer();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            strPlrAns = strPlrAns + noteAt[4].ToString();
+            btn4.BackColor = System.Drawing.Color.FromArgb(82, 179, 217);
+            btn4.Enabled = false; count++; if (count == n) checkAnswer();
+        }
+
+        //
+        // Back to Menu
+        //
+        public delegate void truyen(int value);
+        public truyen trans;
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (check == 1) trans.Invoke(1); else trans.Invoke(0);
+            this.Close();
+        }
+    }
+}
